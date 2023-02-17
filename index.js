10b08193897cc056991644e2a7a1c4ee2bbc5e9c
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-02-17 15:45:26 +0000

    Added logic to test questions

--- index.js
+++ index.js
@@ -54,14 +54,70 @@ const questions = [
 
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'projectTitle',
+      },
+      {
+        type: 'input',
+        message: 'What is the discription of your project?',
+        name: 'projectDiscription',
+      },
+      {
+        type: 'input',
+        message: 'Please enter installation instruction:',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'Please enter usage information:',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Please enter contribution guidelines?',
+        name: 'contributing',
+      },
+      {
+        type: 'input',
+        message: 'Please enter test instrutions?',
+        name: 'tests',
+      },
+      {
+        type: 'checkbox',
+        message: 'Choose a license:',
+        name: 'license',
+        choices: ['MIT License', 'Mozilla Public License 2.0', 'Apache License 2.0', `GNU General Public License v3.0`]
+      },
+      {
+        type: 'input',
+        message: 'Please enter your GitHub username:',
+        name: 'username',
+      },
+      {
+        type: 'input',
+        message: 'Please enter your email address:',
+        name: 'email',
+      },
+  ])
+  .then((response) => {
+   
 
-// function to initialize program
-function init() {
+      console.log('Success!');
 
-}
+  });
 
-// function call to initialize program
-init();
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
