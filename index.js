3d091c18ce0f59ae797ece4254eacc41ba2337d0
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-02-17 15:35:06 +0000

    Added questions array

--- index.js
+++ index.js
@@ -5,6 +5,52 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+      {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'projectTitle',
+      },
+      {
+        type: 'input',
+        message: 'What is the discription of your project?',
+        name: 'projectDiscription',
+      },
+      {
+        type: 'input',
+        message: 'Please enter installation instruction:',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'Please enter usage information:',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Please enter contribution guidelines?',
+        name: 'contributing',
+      },
+      {
+        type: 'input',
+        message: 'Please enter test instrutions?',
+        name: 'tests',
+      },
+      {
+        type: 'checkbox',
+        message: 'Choose a license:',
+        name: 'license',
+        choices: ['MIT License', 'Mozilla Public License 2.0', 'Apache License 2.0', `GNU General Public License v3.0`]
+      },
+      {
+        type: 'input',
+        message: 'Please enter your GitHub username:',
+        name: 'username',
+      },
+      {
+        type: 'input',
+        message: 'Please enter your email address:',
+        name: 'email',
+      },
 
 ];
 
